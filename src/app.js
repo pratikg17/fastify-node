611de08cf017ab33547328ac242b5c7b3317b708
Pratik Gawand <gawand.p@northeastern.edu>
2022-02-16 15:54:26 -0500

    Added Migratiin

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const fastify = require('fastify');
+const db = require('../plugin/database');
 
 const build = (opts = {}) => {
   const app = fastify(opts);
+
+  app.register(db);
 
   app.get('/', async (request, reply) => {
     reply.code(200).send({
