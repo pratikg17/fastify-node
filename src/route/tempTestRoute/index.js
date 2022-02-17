74d9c22c64584ab0431aa78ac6d89195e709cfe4
Pratik Gawand <gawand.p@northeastern.edu>
2022-02-17 10:46:46 -0500

    Added JSON schema for Req and Res

--- src/route/tempTestRoute/index.js
+++ src/route/tempTestRoute/index.js
@@ -1,19 +1,39 @@
 const tempService = require('../../service/temp.service');
+const {
+  postRequestbody,
+  postReponse,
+  getResponseBody,
+} = require('./temp.schema');
 
 const route = async (fastify) => {
-  // get route
+  // get route  api/v1/test/
   const { getAll, save } = tempService(fastify);
-  fastify.get('/', async (request, reply) => {
-    const allTest = await getAll();
-    reply.code(200).send(allTest);
-  });
 
-  fastify.post('/', async (request, reply) => {
-    fastify.log.info(`request with body ${request}`);
-    const { title } = request.body;
-    const id = save(title);
-    reply.code(201).send({ id: id });
-  });
+  fastify.get(
+    '/',
+    { schema: { response: getResponseBody } },
+    async (request, reply) => {
+      const allTest = await getAll();
+
+      reply.code(200).send({
+        temps: allTest,
+      });
+    }
+  );
+
+  // get route api/v1/test
+  fastify.post(
+    '/',
+    { schema: { body: postRequestbody, response: postReponse } },
+    async (request, reply) => {
+      fastify.log.info(`request with body ${request}`);
+      const { title } = request.body;
+
+      const id = await save(title);
+
+      reply.code(201).send(id);
+    }
+  );
 };
 
 module.exports = route;
